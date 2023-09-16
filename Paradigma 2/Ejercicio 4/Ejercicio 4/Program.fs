090014a20a2994f1ee259fe461b5cffdda802578
AFuentes1 <anthonyfuentescalvo@gmail.com>
2023-09-16 01:53:11 -0600

    [ADD] Ejercicios 4 y 5

---
+++ Paradigma 2/Ejercicio 4/Ejercicio 4/Program.fs
@@ -0,0 +1,59 @@
+﻿module RutaCorta
+
+open System.Collections.Generic
+
+// Grafo de prueba con pesos
+let grafo = [
+    ("i", [("a", 3); ("b", 6)]);
+    ("a", [("i", 3); ("c", 5); ("d", 6)]);
+    ("b", [("i", 6); ("c", 2); ("d", 7)]);
+    ("c", [("a", 5); ("b", 2); ("x", 4)]);
+    ("d", [("a", 6); ("b", 7); ("f", 8)]);
+    ("f", [("d", 8)]);
+    ("x", [("c", 4)])
+]
+
+// Función para generar vecinos con pesos
+let rec vecinos nodo grafo =
+    match grafo with
+    | [] -> []
+    | (head, neighbors) :: rest ->
+        if head = nodo then neighbors
+        else vecinos nodo rest
+
+// Función para extender una ruta con pesos
+let extender ruta grafo = 
+    List.collect 
+        (fun (x, peso) -> 
+            let vecinos_con_pesos = vecinos x grafo in
+            List.map (fun (n, p) -> (n, p + peso)) vecinos_con_pesos
+        ) ruta
+
+// Función principal de búsqueda en profundidad con pesos
+let rec prof2 ini fin grafo =
+    let rec prof_aux fin ruta grafo =
+        if List.isEmpty ruta then []
+        elif (fst (List.head ruta)) = fin then
+            [List.rev ruta] @ prof_aux fin (List.tail ruta) grafo
+        else
+            prof_aux fin ((extender ruta grafo) @ (List.tail ruta)) grafo
+    prof_aux fin [(ini, 0)] grafo
+
+// Función para encontrar el camino más corto con pesos
+let camino_mas_corto ini fin grafo =
+    let rutas = prof2 ini fin grafo in
+    match rutas with
+    | [] -> None
+    | _ ->
+        let rutas_con_pesos = List.map (fun ruta -> (ruta, List.sumBy snd ruta)) rutas in
+        let ruta_mas_corta = List.minBy snd rutas_con_pesos in
+        Some ruta_mas_corta
+
+// Ejemplo de uso
+let resultado = camino_mas_corto "i" "f" grafo
+match resultado with
+| Some (ruta, peso) ->
+    printfn "Camino más corto de 'i' a 'f':"
+    List.iter (fun (nodo, p) -> printfn "Nodo: %s, Peso: %d" nodo p) ruta
+    printfn "Peso total: %d" peso
+| None -> printfn "No se encontró un camino de 'i' a 'f'"
---
+++ Paradigma 2/Ejercicio 5/Ejercicio 5/Program.fs
@@ -0,0 +1,54 @@
+﻿//Ejercicio 5 
+type Point = { X: int; Y: int }
+
+let isWithinBounds (laberinto: int list list) (point: Point) =
+    let numRows = List.length laberinto
+    if numRows = 0 then
+        false
+    else
+        let numCols = List.length laberinto.[0]
+        point.X >= 0 && point.X < numRows && point.Y >= 0 && point.Y < numCols
+
+let findShortestPath (laberinto: int list list) (start: Point) (endPos: Point) =
+    let directions = [(-1, 0); (1, 0); (0, -1); (0, 1)]
+
+    let rec dfs currentPos visitedPath =
+        if currentPos = endPos then
+            Some (List.rev visitedPath)
+        else
+            let isValidNeighbor (dx, dy) =
+                let neighbor = { X = currentPos.X + dx; Y = currentPos.Y + dy }
+                isWithinBounds laberinto neighbor && laberinto.[neighbor.X].[neighbor.Y] = 0 && not (List.contains neighbor visitedPath)
+
+            let mutable foundPath = None
+
+            for (dx, dy) in directions do
+                let neighbor = { X = currentPos.X + dx; Y = currentPos.Y + dy }
+                if isValidNeighbor (dx, dy) then
+                    let newPath = neighbor :: visitedPath
+                    foundPath <- dfs neighbor newPath
+
+            foundPath
+
+    if not (isWithinBounds laberinto start) || not (isWithinBounds laberinto endPos) then
+        None
+    elif laberinto.[start.X].[start.Y] = 1 || laberinto.[endPos.X].[endPos.Y] = 1 then
+        None
+    else
+        dfs start [start]
+
+let start = { X = 0; Y = 0 }
+let endPos = { X = 5; Y = 5 }
+
+let laberinto = [
+    [0; 0; 0; 1; 0; 0];
+    [1; 1; 0; 1; 0; 1];
+    [0; 0; 0; 0; 0; 0];
+    [1; 0; 1; 1; 1; 1];
+    [0; 0; 0; 0; 0; 0];
+    [0; 1; 1; 0; 1; 1];
+]
+
+match findShortestPath laberinto start endPos with
+| Some path -> printfn "Ruta más corta encontrada: %A" path
+| None -> printfn "No se encontró una ruta válida."
